c8f3d33958e7cbcf47ef6950158dc06c812d733c
Aadhav <aadhav31@gmail.com>
2026-07-12 22:36:28 +0530

    feat: change portal font to Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Poppins', 'system-ui', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
       },
     },
